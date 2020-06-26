4cd4113e39e3d1e68becf307b4195a9654af168d
steve <stevepesce879@gmail.com>
2020-06-26 10:11:59 -0400

    finished more view stuff on member

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,38 @@
 class MembersController < ApplicationController
+  before_action :set_member, only: [:show,:edit,:destroy,:update]
+  def index
+    @members = Member.all
+  end  
+  
+  def show
+  end
+  
+  def new
+    @member = Member.new    
+  end
+  
+  def create
+    @member = Member.create(member_params)
+    redirect_to member_path(@member)
+  end
+  
+  def edit
+  end
+  
+  def update
+    @member.update(member_params)
+    redirect_to member_path(@member)    
+  end
+  
+  def destroy
+    @member.destroy
+    redirect_to members_path    
+  end
+  private
+  def member_params
+    params.require(:member).permit(:first_name,:last_name)
+  end
+  def set_member
+    @member = Member.find(params[:id])
+  end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,9 @@
 class Member < ApplicationRecord
   has_many :gym_members
   has_many :gyms, through: :gym_members
+  
+  def to_s
+    self.first_name + " " + self.last_name
+  end
+  
 end
